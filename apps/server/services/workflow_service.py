2d5ffbb348952cd559828a6d3543c5854cfe1852
uyeong <uyeong1575@gmail.com>
2026-01-12 10:11:59 +0900

    chore: 주석 수정

--- apps/server/services/workflow_service.py
+++ apps/server/services/workflow_service.py
@@ -21,7 +21,7 @@ class WorkflowService:
         Args:
             db: 데이터베이스 세션
             request: 워크플로우 생성 요청 (app_id, name, description)
-            user_id: 생성자 ID (UUID) # Updated docstring
+            user_id: 생성자 ID (UUID)
         Returns:
             생성된 Workflow 객체
         """
